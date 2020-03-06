1ef5508ea133e0590c775844836d85f56d93bf12
hduWendy <576939544@qq.com>
2020-03-06 15:20:55 +0800

    初步完成

--- 9_circle_Line_Rectangular/9_circle_Line_Rectangular.cpp
+++ 9_circle_Line_Rectangular/9_circle_Line_Rectangular.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
 
 int main()
 {
-    return 0;
+	//圆
+	cv::Point pt,pt1,pt2;
+	cv::Mat dispMat = imread("C:\\Users\\lenovo\\Desktop\\推文\\1.jpg");
+	pt.x = 10;
+	pt.y = 10;
+	circle(dispMat, pt, 10, CV_RGB(255, 255, 255), -1, 8, 0);
+	
+	//线段
+	pt1.x = 10;
+	pt1.y = 10;
+	pt2.x = 30;
+	pt2.y = 30;
+	line(dispMat, pt1, pt2, CV_RGB(255, 0, 0), 1, 8, 0);
+	
+	//矩形框
+	cv::Rect rect;
+	rect.x = 10;
+	rect.y = 50;
+	rect.width;
+	rect.height;
+	rectangle(dispMat, rect, CV_RGB(255, 0, 0), -1, 8, 0);
+
+	cv::imshow("dispMat", dispMat); //展示图片
+/*	cv::imshow("dispMat2", dispMat2); //展示图片
+	cv::imshow("dispMat3", dispMat3); //展示图片
+	*/
+	waitKey(0);
+	return 0;
 }
 
